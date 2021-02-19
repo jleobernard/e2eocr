0809fe8b5de9a2cb3f8ec71643e6ff16fb3bd760
junior bernard <junior.bernard-ext@alantaya.com>
2021-02-19 21:25:52 +0100

    Diminution de la lr

--- src/train.py
+++ src/train.py
@@ -18,7 +18,7 @@ else:
 data_path = sys.argv[1] # '/data/train/one-line'
 models_rep = sys.argv[2] # '/data/models'
 load_model = True
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.000001
 NUM_EPOCHS = int(sys.argv[3]) # 100
 BATCH_SIZE = int(sys.argv[4]) # 100
 HEIGHT = int(sys.argv[5]) # 80
