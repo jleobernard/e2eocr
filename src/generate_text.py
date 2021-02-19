dc1432a52d0179895718173e702f1a920c04e45d
junior bernard <junior.bernard-ext@alantaya.com>
2021-02-19 11:25:31 +0100

    Récuération des données depuis la ligne de commande

--- src/generate_text.py
+++ src/generate_text.py
@@ -1,4 +1,9 @@
 from utils.image_helper import TextGenerator
-
+import sys
+path = sys.argv[1]
+nb_images = sys.argv[2]
+nb_characters = sys.argv[3]
+height = sys.argv[4]
+width = sys.argv[5]
 tg = TextGenerator()
-tg.generate('../data/test/one-line', nb_images=10, nb_characters=5, height=80, width=80)
+tg.generate(path, nb_images=nb_images, nb_characters=nb_characters, height=height, width=width)
