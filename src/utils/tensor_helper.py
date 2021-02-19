ed7168938862db84de8ad97ce0dbb5ff21653def
junior bernard <junior.bernard-ext@alantaya.com>
2021-02-19 11:48:10 +0100

    Tout mettre sur CUDA

--- src/utils/tensor_helper.py
+++ src/utils/tensor_helper.py
@@ -2,7 +2,9 @@ import torch
 
 cuda_available = torch.cuda.is_available()
 
+
 def to_best_device(tensor: torch.Tensor) -> torch.Tensor:
     if cuda_available:
+        print('Sent tensor to CUDA')
         tensor.cuda()
     return tensor
