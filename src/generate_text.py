9a68725872bbb2ecdd1804a535e4bbebafae0afa
junior bernard <junior.bernard-ext@alantaya.com>
2021-02-19 11:28:40 +0100

    Fix command line parsing

--- src/generate_text.py
+++ src/generate_text.py
@@ -1,9 +1,9 @@
 from utils.image_helper import TextGenerator
 import sys
 path = sys.argv[1]
-nb_images = sys.argv[2]
-nb_characters = sys.argv[3]
-height = sys.argv[4]
-width = sys.argv[5]
+nb_images = int(sys.argv[2])
+nb_characters = int(sys.argv[3])
+height = int(sys.argv[4])
+width = int(sys.argv[5])
 tg = TextGenerator()
 tg.generate(path, nb_images=nb_images, nb_characters=nb_characters, height=height, width=width)
