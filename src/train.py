9c07fad243c4fd846311dd263b0d2dac49ccf083
junior bernard <junior.bernard-ext@alantaya.com>
2021-02-20 09:11:50 +0100

    Continue

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ else:
 
 data_path = sys.argv[1] # '/data/train/one-line'
 models_rep = sys.argv[2] # '/data/models'
-load_model = False
+load_model = True
 LEARNING_RATE = 0.00001
 NUM_EPOCHS = int(sys.argv[3]) # 100
 BATCH_SIZE = int(sys.argv[4]) # 100
