fdf5f0ef309daf7f3c3673f0c8b186d27a59adae
leo <junior@alantaya.com>
2021-02-25 12:50:35 +0100

    Fix import

--- src/model/mdlstm_conv_block.py
+++ src/model/mdlstm_conv_block.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from model.mdlstm import MDLSTM
 from utils.tensor_helper import to_best_device
 
-from src.utils.tensor_helper import get_dim_out
+from utils.tensor_helper import get_dim_out
 
 
 class MDLSTMConvBlock(nn.Module):
